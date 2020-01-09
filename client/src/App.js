fee1c791a3d61049f0d59868f055a8e7d4a767c0
Alex K <damned182@mail.ru>
2020-01-09 17:27:34 +0300

    Launch details

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql'
@@ -12,14 +14,17 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src="https://www.spacex.com/sites/spacex/files/spacex_logo_white.png"
-          style={{ width: 300, display: 'block', margin: 'auto' }}
-          alt="Home"
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img
+            src="https://www.spacex.com/sites/spacex/files/spacex_logo_white.png"
+            style={{ width: 300, display: 'block', margin: 'auto' }}
+            alt="Home"
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
