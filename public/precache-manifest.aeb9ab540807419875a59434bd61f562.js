1bd51b23be6e855e3ae59f13b6c4e397e7336194
Alex K <damned182@mail.ru>
2020-01-09 17:35:21 +0300

    compile client to public

---
+++ public/precache-manifest.aeb9ab540807419875a59434bd61f562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db23449628fb18c0ff602115d4e33c15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a5a46785d565462d21f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40d321eb06491ee56909",
+    "url": "/static/js/2.00dabd41.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.00dabd41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a5a46785d565462d21f",
+    "url": "/static/js/main.a2be458b.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
